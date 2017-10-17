6703dfe3ad87b4548131ed19fdaa52e02845fa47
Marco Zangari <mjzangari@gmail.com>
2017-10-17 15:34:55 -0700

    fibonacci working

--- series.py
+++ series.py
@@ -1,4 +1,8 @@
 """This is function exercise based on the fibonacci sequence."""
 
-def fibonacci():
-	#does the stuff
+def fibonacci(n):
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return fibonacci(n - 1) + fibonacci(n - 2)
