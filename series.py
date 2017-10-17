8df4ccf11b3a676532adb88c3d6a746aef85a6ad
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-10-17 16:31:15 -0700

    Lucas

--- series.py
+++ series.py
@@ -1,8 +1,16 @@
 """This is function exercise based on the fibonacci sequence."""
 
 def fibonacci(n):
-	if n == 0:
-		return 0
-	if n == 1:
-		return 1
-	return fibonacci(n - 1) + fibonacci(n - 2)
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def lucas(n):
+    if n == 2:
+        return 2
+    if n == 1:
+        return 3
+    return lucas(n - 1) + lucas(n - 2)
